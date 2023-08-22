c6a3242464456accb73a581ad4b4f63c231ea32a
Adhil Salim <bizadhil@gmail.com>
2023-08-22 12:14:51 +0530

    final finish

--- Example/main.py
+++ Example/main.py
@@ -27,42 +27,11 @@ t.circle(190)
 t.pendown()
 t.end_fill()
 
-# Draw filled triangles around a circle
-
-square_side_length = 50
-t.fillcolor("red")
-
-# t.penup()
-# t.goto(0, 110)
-# t.pendown()
-# t.begin_fill()
-
-# # turn pen to 45 degree
-# t.left(45)
-
-# for _ in range(4):
-#     t.forward(square_side_length)  # Move the turtle forward by side_length units
-#     t.left(90)  # Turn left by 90 degrees
-# t.end_fill()
-
-# t.left(225)
-
-# t.penup()
-# t.goto(25, 110)
-# t.pendown()
-# t.begin_fill()
-
-# # turn pen to 45 degree
-# t.left(45)
-
-# for _ in range(4):
-#     t.forward(square_side_length)  # Move the turtle forward by side_length units
-#     t.left(90)  # Turn left by 90 degrees
-# t.end_fill()
-
+# Draw filled triangles inside the circle
 radius = 90
 num_triangles = 12
-# Draw the triangles
+
+# layer 1
 for _ in range(num_triangles):
     t.penup()
     t.goto(0, 0)
@@ -80,9 +49,9 @@ for _ in range(num_triangles):
     t.left(360 / num_triangles)
     t.pendown()
 
+# layer 2
 radius = 80
 t.left(45)
-# Draw the triangles
 for _ in range(num_triangles):
     t.penup()
     t.goto(0, 0)
@@ -100,9 +69,9 @@ for _ in range(num_triangles):
     t.left(360 / num_triangles)
     t.pendown()
 
+# layer 3
 radius = 70
 t.left(45)
-# Draw the triangles
 for _ in range(num_triangles):
     t.penup()
     t.goto(0, 0)
@@ -120,9 +89,9 @@ for _ in range(num_triangles):
     t.left(360 / num_triangles)
     t.pendown()
 
+# layer 4
 radius = 60
 t.left(45)
-# Draw the triangles
 for _ in range(num_triangles):
     t.penup()
     t.goto(0, 0)
@@ -139,21 +108,24 @@ for _ in range(num_triangles):
     t.penup()
     t.left(360 / num_triangles)
     t.pendown()
-# # Draw the circle 3
-# t.goto(0, -80)
-# t.pendown()
-# t.begin_fill()
-# t.fillcolor("red")
-# t.circle(80)
-# t.end_fill()
 
-# # Draw the circle 4
-# t.goto(0, -70)
-# t.pendown()
-# t.begin_fill()
-# t.fillcolor("yellow")
-# t.circle(70)
-# t.end_fill()
+# Draw the circle 3
+t.penup()
+t.goto(55, 55)
+t.begin_fill()
+t.fillcolor("#f6ff06")
+t.circle(80)
+t.pendown()
+t.end_fill()
+
+# Draw the circle 4
+t.penup()
+t.goto(40, 40)
+t.begin_fill()
+t.fillcolor("#ff6401")
+t.circle(60)
+t.pendown()
+t.end_fill()
 
 # Hide the turtle
 t.hideturtle()
